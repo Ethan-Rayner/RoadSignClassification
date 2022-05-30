5ad3e985d98de385580c22a0a75d64b6a8f39465
Ethan-Rayner <s3902240@student.rmit.edu.au>
2022-05-30 14:05:03 +1000

    trying to condense MLP impolementation to mlp

---
+++ models/MLP.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import pandas as pd
+
+
+
+def fit_model(data, class_column, train_generator, val_generator, image_size, epochs):
+    #MLP
+    
+    INPUT_DIM = (image_size,image_size,3)
+    OUTPUT_CLASSES = len(pd.unique(data[class_column]))
+    HIDDEN_LAYER_DIM = 112
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=INPUT_DIM),
+        tf.keras.layers.Dense(HIDDEN_LAYER_DIM, activation='sigmoid'),
+        tf.keras.layers.Dense(HIDDEN_LAYER_DIM, activation='sigmoid'), #Maybe I can change the hidden layer to see if i can make it more accurate
+        tf.keras.layers.Dense(OUTPUT_CLASSES)
+    ])
+
+    model.compile(optimizer='adam', #this used to be SGD and idk if i can just change it to adam without changing other stuff but the results look better
+              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+              metrics=['categorical_accuracy'])
+    
+    history = model.fit(train_generator, validation_data = val_generator, epochs=epochs, verbose=0)
+    
+    return model, history.history
